06ff77d8efdf56e98e97cb69d1cccccadce81033
KH <1968135+ucefkh@users.noreply.github.com>
2022-12-06 11:53:58 +0100

    no need to have code on each line

--- main.py
+++ main.py
@@ -16,7 +16,9 @@ def compile_chml(chml_code):
 chml_code = '''
 div#app.container:
   h1#heading.primary-heading:Welcome to our website/:
-  p#intro-text.lead:Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.:em#important-note:Please note that all information on this website is for educational purposes only and should not be used as a substitute for professional advice./:  
+  p#intro-text.lead:
+    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+    :em#important-note:Please note that all information on this website is for educational purposes only and should not be used as a substitute for professional advice./:  
   '''
 
 print(compile_chml(chml_code))
